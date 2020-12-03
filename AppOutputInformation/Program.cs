ef3afafed590b8b5f0bccf538f9e9a2c0ab80cbb
silakovvv <75270532+silakovvv@users.noreply.github.com>
2020-12-03 21:43:19 +0300

    init: new app output information

---
+++ AppOutputInformation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppOutputInformation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TMS_03-12");
+        }
+    }
+}
