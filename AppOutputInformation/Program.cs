196a091cfcff18c15405550af566fe060b36742d
silakovvv <75270532+silakovvv@users.noreply.github.com>
2020-12-03 21:55:37 +0300

    feat: add information

--- AppOutputInformation/Program.cs
+++ AppOutputInformation/Program.cs
@@ -7,6 +7,8 @@ namespace AppOutputInformation
         static void Main(string[] args)
         {
             Console.WriteLine("TMS_03-12");
+            Console.WriteLine("Ich lerne C#");
+            Console.WriteLine("Ich muss irgendewas schreiben");
             Console.ReadLine();
         }
     }
